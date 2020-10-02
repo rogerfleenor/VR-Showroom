8ffa44ca2920593736e6cd58006860d76c23a395
Roger Fleenor <rogerfleenor@gmail.com>
2020-10-01 20:29:49 -0400

    testing git hub pages

---
+++ Certified 2020 BMW 750i xDrive for sale in Decatur, GA 30033 _ Kelley Blue Book_files/cookie-first.js
@@ -0,0 +1,4 @@
+
+var domain = location.hostname.match(/(\w+\.\w+)$/) || [location.hostname];
+document.cookie="abc=5rD20r3BrkADvueae2Kqj3kB; path=/; domain=" + domain[0] + "; expires=Sat, 02 Oct 2021 00:05:01 GMT";
+document.cookie="pxa_ipv4=73.106.73.19; path=/; domain=" + domain[0] + "; expires=Sat, 02 Oct 2021 00:05:01 GMT";
